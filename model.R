5ce4cc2f7798c7e273dc3ff2b5517a6e98d954f2
Robert Allaway <allaway@users.noreply.github.com>
2020-02-06 13:46:27 -0800

    Update model.R

--- model.R
+++ model.R
@@ -2,7 +2,7 @@ set.seed(1)
 library(dplyr)
 library(readr)
 
-template <- readr::read_csv('/template.csv') 
+template <- readr::read_csv('/input/template.csv') 
 
 ##model code...we are going to predict everything randomly between 0 and 1 for this example
 
@@ -10,5 +10,5 @@ predictions <- template %>% #get template
   mutate_at(vars(-target), ~ runif(n = nrow(template))) #insert random value in every column except for Patient_ID (do not modify that column)
 
 ## output your predictions to "/predictions.csv" in your container
-predictions %>% write_csv('/predictions.csv')
+predictions %>% write_csv('/output/predictions.csv')
 
